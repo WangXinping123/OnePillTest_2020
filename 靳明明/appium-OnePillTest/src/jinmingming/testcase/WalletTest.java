3f70b69cf64ae987541505b2f2398a4348f8765d
JINmingmingMM <2538513939@qq.com>
2020-05-11 21:59:47 +0800

    jin

--- 靳明明/appium-OnePillTest/src/jinmingming/testcase/WalletTest.java
+++ 靳明明/appium-OnePillTest/src/jinmingming/testcase/WalletTest.java
@@ -20,26 +20,31 @@ public class WalletTest extends BaseTest{
 		LoginPage login=new LoginPage(driver);
 		 homePage=login.loginSuccess("18831107935", "123456");
 		 Thread.sleep(2000);
+		//点击我的
+		homePage.Main();
+		Thread.sleep(3000);
 	}
 	SoftAssert softAssert = new SoftAssert();
 	//点击进入钱包
 	@Test(priority=0)
-	public void Wallet(){
+	public void Wallet() throws Exception{
 		homePage.walletIn();
 		softAssert.assertNotNull("余额");
+		Thread.sleep(2000);
 	}
 	//提现
 	@Test(priority=1)
-	public void MoneyOut(){
+	public void MoneyOut() throws Exception{
 		homePage.Out();
+		Thread.sleep(2000);
 	}
 	//充值
-	@Test(priority=1)
+	@Test(priority=2)
 	public void MoneyIn(){
 		homePage.In();
 	}
 	//返回
-	@Test(priority=1)
+	@Test(priority=3)
 	public void back(){
 		homePage.backWallet();
 		softAssert.assertNotNull("我的");
