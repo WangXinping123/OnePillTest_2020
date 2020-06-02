51381f90bdc046f7fa476b41e1c8610d30c55394
ConyWangYuhan <872916900@qq.com>
2020-06-02 20:20:14 +0800

    monkey

---
+++ 王语晗/Monkey/monkey.bat
@@ -0,0 +1,1 @@
+adb shell monkey -p com.onepilltest   --pct-touch  40 --pct-motion  30 --throttle  300 --ignore-crashes  --ignore-timeouts --ignore-security-exceptions --ignore-native-crashes  --monitor-native-crashes  -v -v  -v 1000  1>E:\GradeThreeTerm2\AARealityTraining\Monkey\monkey1.log
